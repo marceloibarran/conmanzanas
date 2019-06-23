26fb985d603d8ff8c07e06dc8820cc90fe4b0046
Diego <diego.san.lan@gmail.com>
2019-06-22 21:36:11 -0400

    scraping preciomundi v0.5
    ya obtiene la mayoria de datos de la pagina necesarios

--- Scrap/preciomundo.py
+++ Scrap/preciomundo.py
@@ -1,13 +1,62 @@
 import requests
-import time
 from bs4 import BeautifulSoup
+from datetime import datetime, date
+
 
 
 paises = ['chile']
 product = ['precios-supermercado']
-lista_pais_peso = {'chile':'peso chileno'}
+
+#diccionario de paises con tipo de moneda peso
+lista_pais_peso = {'chile':'Peso Chileno', 'argentina': 'Peso Argentino', 'uruguay': 'Peso Uruguayo', 'mexico': 'Peso Mexicano' }
+validador = 0
+
+#funcion que obtiene el tipo de moneda
+def tipo_moneda(tipo_precio):
+    total_cadena = len(tipo_precio)
+    direc = total_cadena - 1
+    while tipo_precio[direc] != ' ':
+        direc = direc - 1
+        tipo_valor_filtrado = tipo_precio[0:direc]
+        pass
+    return tipo_valor_filtrado
+    pass
+
+
+def busca_mes(mes):
+    diccionario = {'enero': '01', 'febrero':'02','marzo':'03','abril':'04','mayo':'05','junio':'06','julio':'07','agosto':'08','septiembre':'09','obtubre':'10','noviembre':'11','diciembre':'12'}
+    for mes_list in diccionario:
+        if mes == mes_list:
+            return diccionario[mes_list]
+        pass
+    return '01'
+    pass
+
+#funcion que genera fecha de actualizacion
+def fecha_actializacion(fecha):
+    total_cadena = len(fecha)
+    direc = 0
+    fecha= fecha[16:total_cadena-1]
+
+    while fecha[direc] != ' ':
+        direc = direc + 1
+        mes = fecha[0:direc]
+        ano = fecha[direc+4:]
+    fecha_final = ano+'-'+busca_mes(mes)+'-01'
+    return fecha_final
+    pass
 
 for pais in paises:
+
+    for pais_lista in lista_pais_peso:
+        if pais == pais_lista:
+            #cambia a 1 cuando es un tipo de pais con moneda tipo peso
+            validador = 1
+            tipo_valor_filtrado= lista_pais_peso[pais_lista]
+            pass
+        pass
+
+
 
     url = 'https://preciosmundi.com/' + pais
     for i in product:
@@ -15,35 +64,38 @@ for pais in paises:
         r = requests.get(ur)
 
         soup = BeautifulSoup(r.content, "html.parser")
+        fecha_actualizacion_final = fecha_actializacion(soup.find('p').get_text().strip())
+        print(fecha_actualizacion_final)
+
         tbody_total = soup.find_all('tbody')
 
-        #se obtiene el tipo de moneda
-        tipo_precio = soup.find_all('th')[1].get_text().strip()
-        total_cadena= len(tipo_precio)
-        direc = total_cadena-1
-        while tipo_precio[direc] != ' ':
-            direc = direc - 1
-            tipo_valor_filtrado = tipo_precio[0:direc]
-            pass
 
+        #valida si pais es con moneda peso
+        if validador == 0:
+            #se obtiene el tipo de moneda
+            tipo_valor_filtrado = tipo_moneda(soup.find_all('th')[1].get_text().strip())
 
         for tbody in tbody_total:
             tr_tabla = tbody.find_all('tr')
 
             for tr_dato in tr_tabla:
-                fecha_actualizacion = soup.find('p').get_text().strip()
+
                 nombre = tr_dato.find('td', class_='product-name').get_text().strip()
                 precio_peso = tr_dato.find_all('td', class_='price')[0].get_text().strip()
                 cantidad = len(precio_peso)
                 precio_peso = precio_peso[:cantidad - 1]
+
+                fecha_obtencion_dato = date.today()
+                now = datetime.now()
+                hora_obtencion = now.strftime("%H:%M:%S")
+
                 print(nombre)
-                print(tipo_valor_filtrado)
                 print(precio_peso)
-                print("-------------------")
-
-
-
-
+                print(tipo_valor_filtrado)
+                print(fecha_actualizacion_final+' fecha de actualizacion')
+                print(fecha_obtencion_dato)
+                print(hora_obtencion)
+                print('-----------')
 
 
 
