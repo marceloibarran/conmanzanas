88438d79977b126c8b81edd2c417f2f24bce6e25
marceloibarra <marceloibarra06.com>
2019-06-26 15:18:11 -0400

    base de datos auto
    base de datos lista para pruebas

--- basedatos/models.py
+++ basedatos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Bienesyservicios(models.Model):
-   idbs = models.PositiveIntegerField(primary_key=True)
+   idbs = models.AutoField(primary_key=True)
    nombrebs = models.TextField(max_length=250)
    precio = models.DecimalField(max_digits=5, decimal_places=3)
    imgc = models.ImageField(upload_to='bysimg')
@@ -13,23 +13,23 @@ class Bienesyservicios(models.Model):
    idsubc = models.ForeignKey('Subcategoria', on_delete=models.PROTECT)
 
 class Region(models.Model):
-   idreg = models.PositiveIntegerField(primary_key=True)
+   idreg = models.AutoField(primary_key=True)
    nombreg = models.TextField(max_length=200)
    idpais = models.ForeignKey('Pais', on_delete=models.PROTECT)
 
 class Pais(models.Model):
-   idpais = models.PositiveIntegerField(primary_key=True)
+   idpais = models.AutoField(primary_key=True)
    nombrep = models.TextField(max_length=100)
 
 class Subcategoria(models.Model):
-   idsubc = models.PositiveIntegerField(primary_key=True)
+   idsubc = models.AutoField(primary_key=True)
    nomsc = models.TextField(max_length=200)
    idcat = models.ForeignKey('Categoria', on_delete=models.PROTECT)
 
 class Categoria(models.Model):
-   idcat = models.PositiveIntegerField(primary_key=True)
+   idcat = models.AutoField(primary_key=True)
    nomcat = models.TextField(max_length=200)
 
 class Tipocambio(models.Model):
-   id_cambio = models.PositiveIntegerField(primary_key=True)
+   id_cambio = models.AutoField(primary_key=True)
    nombrecamp = models.TextField(max_length=20)
