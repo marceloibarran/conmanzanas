794ed09e9e9016fa79722ff36123fd371dbe03e6
Jaime Morales <jaime.morales@alumnos.upla.cl>
2019-06-27 14:15:43 -0400

    subir datos y regiones en funciones, regiones falta insert

--- Scrap/subirDatos.py
+++ Scrap/subirDatos.py
@@ -1,12 +1,23 @@
 import sqlite3
 import json
-paises = json.load(open('Paises.json'))
-for pais in paises:
-    paist= pais['Pais']
-    con = sqlite3.connect('/home/jaimemor/Escritorio/Taller/Proyecto/conmanzanas/Scrap/db.sqlite3')
-    cursor = con.cursor()
-    sql = "INSERT INTO basedatos_pais(idpais, nombrep) VALUES(Null,'"+paist+"')"
-    cursor.execute(sql)
-    con.commit()
-    con.close()
-
+def Subir_Paises():
+    paises = json.load(open('file.json'))
+    for pais in paises:
+        paist= pais['Pais']
+        print(paist)
+        con = sqlite3.connect('/home/jaimemor/Escritorio/Taller/Proyecto/conmanzanas/Scrap/db.sqlite3')
+        cursor = con.cursor()
+        sql = "INSERT INTO basedatos_pais(idpais, nombrep) VALUES(Null,'"+paist+"')"
+        cursor.execute(sql)
+        con.commit()
+        con.close()
+def Subir_Region():
+    regiones = json.load(open('file.json'))
+    for region in regiones:
+        regiont= region['Continente']
+        con = sqlite3.connect('/home/jaimemor/Escritorio/Taller/Proyecto/conmanzanas/Scrap/db.sqlite3')
+        cursor = con.cursor()
+        #sql= "INSERT INTO basedatos_region(idreg, nombreg, idpais_id) VALUES(Null,'""')"
+        cursor.execute(sql)
+        con.commit()
+        con.close()
