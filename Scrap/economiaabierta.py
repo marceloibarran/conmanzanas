f614c114b3d60d764b5bc3b31e955a65b0448b0a
Diego <diego.san.lan@gmail.com>
2019-06-29 17:55:55 -0400

    modificacion en economiaabierta.py

--- Scrap/economiaabierta.py
+++ Scrap/economiaabierta.py
@@ -15,33 +15,38 @@ list_precio_medicamento = json.loads(resp.content)
 
 page = 0
 validador = 0
-for x in list_precio_medicamento['resources']:
-    guid = x['guid']
+
+ultimo = list_precio_medicamento['resources'][0]
+guid = ultimo['guid'].strip()
 
 
 
-    while validador == 0:
-        link = "https://api.datos.sernac.cl/api/v2/datastreams/" + guid + "/data.pjson/?auth_key=edbb95d24d7caeeb9dc6a0da35bc5a3c1fed957d&limit=50&page="+str(page)
+while validador == 0:
+    link = "https://api.datos.sernac.cl/api/v2/datastreams/" + guid + "/data.pjson/?auth_key=edbb95d24d7caeeb9dc6a0da35bc5a3c1fed957d&limit=50&page=" + str(
+        page)
 
-        lista_producto = requests.get(link)
-        lista_producto_json = json.loads(lista_producto.content)
-        if len(lista_producto_json['result']) == 1:
-            validador = 1
-            pass
-        page = page + 1
-        for c in range(0, 49):
-            lista_medicamento = lista_producto_json['result'][c]
-            nombre = lista_medicamento['Glosa_Producto-Principio-Activo']
-            fecha_publicacion = modifica_fecha_publicacion(lista_medicamento['Fecha'])
-            fecha_hoy = date.today()
-            fuente = 'https://datos.sernac.cl/dashboards/20611/precios-medicamentos/'
+    lista_producto = requests.get(link)
+    lista_producto_json = json.loads(lista_producto.content)
+    if len(lista_producto_json['result']) == 1:
+        validador = 1
+        pass
+    page = page + 1
+    for c in range(0, 49):
+        lista_medicamento = lista_producto_json['result'][c]
+        nombre = lista_medicamento['Glosa_Producto-Principio-Activo']
+        precio = 3
 
-            print(nombre)
-            print(fecha_publicacion)
-            print(fecha_hoy)
-            print(fuente)
+        fecha_publicacion = modifica_fecha_publicacion(lista_medicamento['Fecha'])
 
-            pass
+        fecha_hoy = date.today()
+        fuente = 'https://datos.sernac.cl/dashboards/20611/precios-medicamentos/'
 
-    validador = 0
-    page = 0
+        print(nombre)
+        print(precio)
+        print(fecha_publicacion)
+        print(fecha_hoy)
+        print(fuente)
+
+        pass
+
+
